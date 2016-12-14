2371dd6acacfd6048f79d73cf1c6e6e5e5f25b7b
yoch <yoch.melka@gmail.com>
2016-12-14 10:51:50 +0200

    adding load_svmfiles to load multiples files and infer nfeatures bugfixes

--- svmloader.pyx
+++ svmloader.pyx
@@ -1,25 +1,28 @@
 cimport cython
-import numpy as np
 from libc.stdlib cimport strtoul, strtod
 from libc.stdint cimport uint32_t
+import array
 from cpython cimport array
+import numpy as np
 import scipy.sparse as sp
 
 
 @cython.boundscheck(False) # turn off bounds-checking for entire function
 @cython.wraparound(False)  # turn off negative index wrapping for entire function
-@cython.initializedcheck(False)
+#@cython.initializedcheck(False)
 cdef _load_svmfile(fp, bint zero_based):
     cdef char * s
     cdef char * end
-    cdef uint32_t idx, sz
+    cdef uint32_t idx
     cdef float value
-    cdef bytes label, rest
+    cdef bytes label
+    cdef bytes rest
+    cdef Py_ssize_t sz
 
-    cdef array.array[float] data = array('f')
-    cdef array.array[uint32_t] indices = array('L')
-    cdef array.array[uint32_t] indptr = array('L', [0])
-    cdef array.array[int] labels = array('l')
+    cdef array.array data = array.array('f')
+    cdef array.array indices = array.array('L')
+    cdef array.array indptr = array.array('L', [0])
+    cdef array.array labels = array.array('l')
 
     sz = 0
     for line in fp:
@@ -28,7 +31,7 @@ cdef _load_svmfile(fp, bint zero_based):
 
         # get the label
         label, rest = line.split(maxsplit=1)
-        labels.resize_smart(len(labels)+1)
+        array.resize_smart(labels, len(labels)+1)
         labels[len(labels)-1] = int(label)
         s = rest
 
@@ -50,25 +53,24 @@ cdef _load_svmfile(fp, bint zero_based):
             if not zero_based:
                 idx -= 1
 
-            indices.resize_smart(sz+1)
-            data.resize_smart(sz+1)
+            array.resize_smart(indices, sz+1)
+            array.resize_smart(data, sz+1)
             indices[sz] = idx
             data[sz] = idx
             sz += 1
 
-        indptr.resize_smart(len(indptr)+1)
+        array.resize_smart(indptr, len(indptr)+1)
         indptr[len(indptr)-1] = len(data)
 
-    X = sp.csr_matrix((data, indices, indptr))
     y = np.asarray(labels)
 
-    return (X, y)
+    return (data, indices, indptr, y)
 
 
 
 import os.path
 
-def load_svmfile(filename, zero_based=True):
+def _openfile(filename):
     _, ext = os.path.splitext(filename)
     if ext == ".gz":
         import gzip
@@ -78,5 +80,35 @@ def load_svmfile(filename, zero_based=True):
         fp = BZ2File(filename, "rb")
     else:
         fp = open(filename, "rb")
+    return fp
 
-    return _load_svmfile(fp, zero_based)
+
+def load_svmfile(filename, nfeatures=None, zero_based=True):
+    fp = _openfile(filename)
+    data, indices, indptr, y = _load_svmfile(fp, zero_based)
+    fp.close()
+
+    if nfeatures is None:
+        X = sp.csr_matrix((data, indices, indptr))
+    else:
+        X = sp.csr_matrix((data, indices, indptr), (len(indptr)-1, nfeatures))
+
+    return X, y
+
+def load_svmfiles(filenames, zero_based=True):
+    Xlst = []
+    ylst = []
+    for filename in filenames:
+        fp = _openfile(filename)
+        data, indices, indptr, y = _load_svmfile(fp, zero_based)
+        Xlst.append((data, indices, indptr))
+        ylst.append(y)
+        fp.close()
+    # get nfeatures as the maximum indice
+    nfeatures = max(max(indices) for _, indices, _ in Xlst) + 1
+    lst = []
+    for (data, indices, indptr), y in zip(Xlst, ylst):
+        X = sp.csr_matrix((data, indices, indptr), (len(indptr)-1, nfeatures))
+        lst.append(X)
+        lst.append(y)
+    return lst
