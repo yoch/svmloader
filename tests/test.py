ee6c719b95837a14e9fdb513eb41ec4e2e08ab02
yoch <yoch.melka@gmail.com>
2016-12-22 17:35:53 +0200

    fix tests

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,10 @@
+import os.path
 from svmloader import *
 
-X, y = load_svmfile('satimage.scale', zero_based=False)
+DIR = os.path.dirname(__file__)
+
+X, y = load_svmfile(os.path.join(DIR, 'satimage.scale'), zero_based=False)
 print(X.shape, X.nnz)
 
-X, y = load_svmfile('yeast_train.svm', zero_based=False, multilabels=True)
+X, y = load_svmfile(os.path.join(DIR, 'yeast_train.svm'), zero_based=False, multilabels=True)
 print(X.shape, X.nnz)
